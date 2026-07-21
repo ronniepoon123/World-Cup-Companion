d6fcf33b30c7965a38e9512c3bc507e6aa344b7f
ronniepoon123 <ronniepoon_rp@hotmail.com>
2026-07-21 19:58:59 +0800

    Fix navbar import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar";
 
 import HomePage from "./pages/HomePage";
 import TeamsPage from "./pages/TeamsPage";
